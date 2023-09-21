37f025261c51830c4a7fbcbcd73eeedb7b19d539
Sajjad <sajjads.hashemi@gmail.com>
2023-09-21 19:56:19 -0400

    Run for more epochs

--- src/models/wandb_train.py
+++ src/models/wandb_train.py
@@ -18,7 +18,7 @@ from training_utils import train, validation, MaterialSimilarityLoss, Exponentia
 
 def run_training(epochs, a_mse, a_content, a_style, a_spst, beta, content_layer, style_layer,
                 learning_rate=1e-3, batch_size=32, CNN_embed_dim=256,
-                  dropout_p=0.2, log_interval=32, save_interval=10, resume_training=False, last_epoch=None):
+                  dropout_p=0.2, log_interval=32, save_interval=10, resume_training=True, last_epoch=30):
     seed=110
     seed_everything(seed)
     
