cff7303d2f9a0b5a7d8797dce42a8d0f236b396e
Sajjad <sajjads.hashemi@gmail.com>
2023-11-29 17:02:15 -0500

    Fix bug

--- src/models/create_images.py
+++ src/models/create_images.py
@@ -5,30 +5,40 @@ import os
 from PIL import Image
 
 def stack_and_save_images(dir_path, epoch):
-    # Hardcoded image names
-    img_names = [f'original_images_epoch{epoch}.npy', f'reconstructed_images_epoch{epoch}.npy', f'original_autocorr_epoch{epoch}.npy', f'reconstructed_autocorr_epoch{epoch}.npy']
+    # Hardcoded image names with dynamic epoch
+    img_names = [
+        f'original_images_epoch{epoch}.npy',
+        f'reconstructed_images_epoch{epoch}.npy',
+        f'original_autocorr_epoch{epoch}.npy',
+        f'reconstructed_autocorr_epoch{epoch}.npy'
+    ]
     imgs = [np.load(os.path.join(dir_path, name)) for name in img_names]
 
-    # Convert to PyTorch tensors
-    imgs = [torch.tensor(img, dtype=torch.float32) for img in imgs]
+    # Convert to PyTorch tensors and remove singleton color channel dimension
+    imgs = [torch.tensor(img, dtype=torch.float32).squeeze(1) for img in imgs]
 
-    # Stack images 1 and 2 vertically, and 3 and 4 vertically
-    stacked1 = torch.cat([imgs[0], imgs[1]], dim=2) # Change axis if needed
-    stacked2 = torch.cat([imgs[2], imgs[3]], dim=2) # Change axis if needed
-
-    # Stack the two pairs horizontally
-    final_img = torch.cat([stacked1, stacked2], dim=3) # Change axis if needed
+    # Stack images vertically and then horizontally
+    stacked1 = torch.cat([imgs[0], imgs[1]], dim=1)  # Stacking along height
+    stacked2 = torch.cat([imgs[2], imgs[3]], dim=1)  # Stacking along height
+    final_img = torch.cat([stacked1, stacked2], dim=2)  # Stacking along width
 
     # Ensure 'results' directory exists
-    results_dir = os.path.join(dir_path, 'results')
+    results_dir = os.path.join(dir_path, 'image_results')
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
 
     # Save each image in the batch
     for i in range(final_img.shape[0]):
-        # Convert tensor to PIL Image for saving
-        img = Image.fromarray(final_img[i].numpy().astype('uint8'))
-        img.save(os.path.join(results_dir, f"result_{i}.jpg"))
+        # Convert tensor to numpy array
+        img_array = final_img[i].numpy()
+        
+        # Convert grayscale to RGB if needed
+        if img_array.ndim == 2:
+            img_array = np.stack((img_array,) * 3, axis=-1)
+
+        # Convert to PIL Image and save
+        img = Image.fromarray(img_array.astype('uint8'))
+        img.save(os.path.join(results_dir, f"result_epoch{epoch}_{i}.jpg"))
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
